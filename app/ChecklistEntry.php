88b3333b29540fc8e520297650ae70fc666f7a0a
Ignacio Miranda <imiranda4305@gmail.com>
2017-06-18 22:22:07 -0400

    Configured fillable in Models for Checklist

--- app/ChecklistEntry.php
+++ app/ChecklistEntry.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChecklistEntry extends Model
 {
-    //
+    protected $fillable = ['id', 'checklist_registry_id', 'checklist_item_id', 'response', 'observations'];
 }
--- app/ChecklistRegister.php
+++ app/ChecklistRegister.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChecklistRegister extends Model
 {
-    //
+    protected $fillable = ['id', 'turn', 'checklist_id', 'user_id', 'credential_avaliable', 'credential_delivered'];
 }
