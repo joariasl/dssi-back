3595ff108d376b4ed1972bda16f4eef5b5e99117
Jorge Arias <mail@jorgearias.cl>
2017-06-19 03:14:15 -0400

    Return ChecklistItemGroup using Eloquent Querying Relationship Existence

--- app/ChecklistItem.php
+++ app/ChecklistItem.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ChecklistItem extends Model
 {
     protected $fillable = ['id', 'checklist_id', 'checklist_item_group_id', 'name', 'status'];
+
+    /**
+     * Get the item for ChecklistGroup.
+     */
+    public function checklistItemGroup()
+    {
+        return $this->belongsTo('App\ChecklistItemGroup');
+    }
 }
