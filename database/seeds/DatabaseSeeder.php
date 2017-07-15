21b7eefa5982a971378154fc69f5ad6cf838c53c
Jorge Arias <mail@jorgearias.cl>
2017-07-15 18:22:21 -0400

    Added seeds for Permissions, Roles
    * Assigned for user jdoe in a UserRole seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,6 +30,9 @@ class DatabaseSeeder extends Seeder
         $this->call(KeysTableSeeder::class);
         $this->call(KeyLoansTableSeeder::class);
         $this->call(ModulesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UserRoleSeeder::class);
 
 
         Model::reguard();
---
+++ database/seeds/UserRoleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::find(1);
+        $user->roles()->attach(Role::where('name', 'access-control.checklist-registry.read')->first()->id);
+        $user->roles()->attach(Role::where('name', 'access-control.checklist-registry.write')->first()->id);
+        $user->roles()->attach(Role::where('name', 'access-control.checklist.admin')->first()->id);
+    }
+}
